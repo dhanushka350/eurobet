be047fd785cf5e8750e83f0f38734626ca505482
dhanushka350 <dhanushka@akvasoft.com>
2018-12-29 14:16:30 +0530

    scrape times

--- src/main/java/com/akvasoft/eurobet/EurobetApplication.java
+++ src/main/java/com/akvasoft/eurobet/EurobetApplication.java
@@ -3,13 +3,19 @@ package com.akvasoft.eurobet;
 import com.akvasoft.eurobet.config.Scrape;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EurobetApplication {
+public class EurobetApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EurobetApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 }
 
